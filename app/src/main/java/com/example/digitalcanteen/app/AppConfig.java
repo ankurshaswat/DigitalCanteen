e19ecd7f70e36bb68c55d91f360a143d9c8b52c6
Shashwat Shivam <ankurshaswat@yahoo.co.in>
2018-03-18 15:52:32 +0530

    Added template API call file

---
+++ app/src/main/java/com/example/digitalcanteen/app/AppConfig.java
@@ -0,0 +1,28 @@
+package com.example.digitalcanteen.app;
+
+/**
+ * Created by ankurshaswat on 10/6/17.
+ */
+
+public class AppConfig {
+
+
+    public static String URL_ADD_UPDATE_COLLECTION = "";
+
+    public static String URL_ADD_TRANSACTION = "";
+    public static String URL_GET_TRANSACTIONS = "";
+
+    public static String URL_ADD_UPDATE_USER = "";
+    //    public static String URL_UPDATE_USER = "";
+    public static String URL_GET_USERS = "";
+
+    public static String URL_ADD_UPDATE_MENU = "";
+    //    public static String URL_UPDATE_MENU = "";
+    public static String URL_DELETE_MENU = "";
+    public static String URL_GET_MENU = "";
+
+    public static String URL_TEST_CONNECTION = "";
+
+
+
+}
